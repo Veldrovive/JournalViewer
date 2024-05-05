62aba92b5db25352a5f214e3a251f9e8859a30cd
Aidan Dempster <aidan.dempster@gmail.com>
2024-05-05 09:26:51 -0400

    Added a fullscreen view for image entries
    Also added the ability to zoom and pan for these fullscreen images

--- renderer/components/entries/_ImageEntry.tsx
+++ renderer/components/entries/_ImageEntry.tsx
@@ -1,6 +1,9 @@
 "use client"
 import { OutputEntry, FileData } from "../../interfaces/entryApiInterfaces"
-import { Image } from "@mantine/core"
+import { useDisclosure } from "@mantine/hooks"
+import { Image, ActionIcon, Modal } from "@mantine/core"
+import { IconMaximize } from "@tabler/icons-react"
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch"
 
 interface ImageEntryProps {
     entry: OutputEntry<FileData>
@@ -9,9 +12,34 @@ interface ImageEntryProps {
 const ImageEntry = ({ entry }: ImageEntryProps) => {
     const imageUrl = entry.data.fileUrl
 
+    const [opened, { open, close }] = useDisclosure(false);
+
+    const goFullScreen = (e) => {
+        e.stopPropagation()  // Prevent the map from updating
+        open()
+    }
+
     return (
-        <div>
+        <div style={{ position: 'relative' }}>
             <Image src={imageUrl} alt={entry.entryType} />
+            <ActionIcon style={{ position: 'absolute', top: 5, right: 5 }} size="lg" onClick={goFullScreen}>
+                <IconMaximize />
+            </ActionIcon>
+            <Modal.Root opened={opened} onClose={close} size="auto" centered>
+                <Modal.Overlay opacity={0.7} blur={3} />
+                <Modal.Content>
+                    <Modal.Body style={{ padding: 0 }}>
+                        <ActionIcon style={{ zIndex: 10, position: 'absolute', top: 5, right: 5 }} size="lg" onClick={close}>
+                            <IconMaximize />
+                        </ActionIcon>
+                        <TransformWrapper>
+                            <TransformComponent>
+                                <Image src={imageUrl} alt={entry.entryType} onClick={e => e.stopPropagation()} />
+                            </TransformComponent>
+                        </TransformWrapper>
+                    </Modal.Body>
+                </Modal.Content>
+            </Modal.Root>
         </div>
     )
 }
